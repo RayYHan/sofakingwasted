9803930c6cd1551f55ea060a43b296024a9606d6
Ray Han <giraiday@gmail.com>
2019-11-28 21:51:46 -0800

    new app changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
 
-function App() {
-  let storage = !firebase.apps.length
-    ? firebase.initializeApp({
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logo: '',
+      shelf_img: '',
+      whiskeys: {},
+    };
+  }
+
+  componentDidMount() {
+    firebase.initializeApp({
       storageBucket: "sofakingwasted-e795e.appspot.com",
-      projectId: "sofakingwasted-e793e"
-    }).storage()
-    : firebase.app().storage();
-  let shelf_img = storage.ref('assets/shelf.png').fullPath;
+      projectId: "sofakingwasted-e795e",
+      databaseURL: "sofakingwasted-e795e.firebaseio.com",
+    });
+    const storage = firebase.storage();
+    const firestore = firebase.firestore();
+    let whiskey_ref = firestore.collection('whiskeys')
+    whiskey_ref.get().then( snapshot => {
+      snapshot.forEach(doc => {
+        let data = doc.data();
+        storage.ref(`assets/whiskeys/${data['image']}`).getDownloadURL().then(res => {
+          this.setState({
+            whiskeys: {...this.state['whiskeys'], [doc.id]: {...data, img_url: res}}
+          });
+        });
+      });
+    }).catch( console.error );
+    storage.ref('assets/logo.svg').getDownloadURL().then(res => {
+      this.setState({ logo: res })
+    });
+    storage.ref('assets/shelf.png').getDownloadURL().then(res => {
+      this.setState({ shelf_img: res });
+    });
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={shelf_img} alt="shelf" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    const numbers = [1, 2, 3, 4, 5];
+    const whiskey_data = Object.values(this.state['whiskeys'])
+    whiskey_data.sort((w1, w2) => Object.values(w1['score']).reduce((s1, s2) => s1 + s2, 0) - Object.values(w2['score']).reduce((s1, s2) => s1 + s2, 0));
+    const whiskeys = whiskey_data.map(whiskey => (
+      <img className="whiskey" src={whiskey['img_url']} alt={whiskey['img_url']} />
+    ))
+    const shelfs = numbers.map(num => (
+      [
+        <div className="whiskeys">{whiskeys}</div>,
+        <img src={this.state['shelf_img']} alt="shelf" id={'shelf' + num} />
+      ]
+    ));
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img className="App-logo" src={this.state['logo']} alt={this.state['logo']} width="85" align="left"/>
+        </header>
+        <main className="App-main">
+          {shelfs}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
